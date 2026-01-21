7e68fe7555bb39514d20cc2246606ece2109fdd4
TAHIR ZAMAN <tahirzaman8585@outlook.com>
2026-01-21 13:45:11 +0500

    Create topic1peactises.py

---
+++ Learning Phase/#1 Foundations/#1 Python/topic1peactises.py
@@ -0,0 +1,35 @@
+# WE Cover these Topics: Only Practises
+# print Function, Input Function, Variables & Data Types
+
+
+
+
+# print Function
+print("Hello, World!") 
+print("Hello", "World")
+
+
+print("Python")
+print("Learning")
+
+
+print("Python", end=" ")
+print("Programming")
+
+print(10 + 20)
+print("10 + 20")
+
+print("A", "B", "C", sep="-")
+
+x = 5
+print(x)
+
+
+
+x = 5
+y = 10
+print(x, y)
+
+
+print("Line1\nLine2")
+
